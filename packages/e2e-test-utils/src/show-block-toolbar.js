7830728717b46a15a2642f4a37f3d998e28c2bf2
Ella van Durpe <ella@vandurpe.com>
2020-11-18 13:58:06 +0200

    Fix show toolbar util

--- packages/e2e-test-utils/src/show-block-toolbar.js
+++ packages/e2e-test-utils/src/show-block-toolbar.js
@@ -3,7 +3,9 @@
  * Call this function to reveal it.
  */
 export async function showBlockToolbar() {
+	const iframe = await page.$( 'iframe[name="editor-canvas"]' );
+	const iframeRect = await iframe.boundingBox();
 	// Move the mouse to disable the isTyping mode
-	await page.mouse.move( 50, 50 );
-	await page.mouse.move( 100, 100 );
+	await page.mouse.move( iframeRect.x + 50, iframeRect.y + 50 );
+	await page.mouse.move( iframeRect.x + 100, iframeRect.y + 100 );
 }
